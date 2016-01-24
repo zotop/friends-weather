60767dc54fef7df6e58cb3f404044974f0d4805b
Joao Guerreiro <jmdguerreiro@gmail.com>
2016-01-24 23:16:12 +0100

    Typhoeus library used for parallel requests to weather api

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,34 @@
+
 class WeatherController < ApplicationController
+   
+  WEATHER_API_KEY = '32a40352b94f9f112f73bc2f2cd13765'
+  WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
   def show
-  	render json: params
+  	@friends = Friend.find params['friends_id']
+  	@friendsWeather = Array.new 
+  	hydra = Typhoeus::Hydra.new
+  	for index in 0 ... @friends.size
+  		location = @friends.at(index)['location']
+		weatherApiUrl = WEATHER_BASE_URL + "?q=" + location + "&appid=" + WEATHER_API_KEY + "&units=metric"
+  		request = Typhoeus::Request.new weatherApiUrl
+    	request.on_complete do |response|
+    		jsonResponse = JSON.parse response.body
+      		temperature = jsonResponse['main']['temp']
+			weatherDescription = jsonResponse['weather'][0]['description'].downcase
+			weatherIcon = jsonResponse['weather'][0]['icon']
+			friendAsHash = @friends.at(index).attributes
+			friendAsHash ['temperature'] = temperature
+			friendAsHash ['weatherDescription'] = weatherDescription
+			friendAsHash ['weatherIcon'] = weatherIcon
+			@friendsWeather.push friendAsHash	
+    	end
+
+  		hydra.queue request
+  	end
+  	hydra.run
+   	 
+
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :friends do
-    collection do 
-      post 'weather'
-    end
-  end
+  resources :friends 
   
   get 'weather' => 'weather#show'
 
@@ -12,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'friends#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
