d3b3b1bd7205d773e7b7eeabca83e005eec7a3a8
Joao Guerreiro <jmdguerreiro@gmail.com>
2016-01-25 14:28:44 +0100

    used skeleton css tables

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,11 +5,11 @@ class WeatherController < ApplicationController
   WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
   def show
-  	@friends = Friend.find params['friends_id']
+  	friends = Friend.find params['friends_id']
   	@friendsWeather = Array.new 
   	hydra = Typhoeus::Hydra.new
-  	for index in 0 ... @friends.size
-  		location = @friends.at(index)['location']
+  	friends.each do |friend|
+  		location = friend['location']
 		weatherApiUrl = WEATHER_BASE_URL + "?q=" + location + "&appid=" + WEATHER_API_KEY + "&units=metric"
   		request = Typhoeus::Request.new weatherApiUrl
     	request.on_complete do |response|
@@ -17,7 +17,7 @@ class WeatherController < ApplicationController
       		temperature = jsonResponse['main']['temp']
 			weatherDescription = jsonResponse['weather'][0]['description'].downcase
 			weatherIcon = jsonResponse['weather'][0]['icon']
-			friendAsHash = @friends.at(index).attributes
+			friendAsHash = friend.attributes
 			friendAsHash ['temperature'] = temperature
 			friendAsHash ['weatherDescription'] = weatherDescription
 			friendAsHash ['weatherIcon'] = weatherIcon
